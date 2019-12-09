49a53c1498521d1ca1e0634f333603cdb3943126
Alexei Shilin <alexei.shilin@gmail.com>
2019-12-09 19:08:11 +0700

    clear up

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,16 +1,6 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Meta from "./Meta";
-import styled from "styled-components";
-
-const MyButton = styled.button`
-  background: red;
-  font-size: 50px;
-  .poop {
-    font-size: ${props => props.huge ? '200px' : '30px' };
-  }
-`;
-
 
 class Page extends Component {
   render() {
@@ -18,9 +8,6 @@ class Page extends Component {
       <div>
         <Meta />
         <Header />
-        <MyButton huge>
-          <span className="poop">💩</span>
-        </MyButton>
         {this.props.children}
       </div>
     );
