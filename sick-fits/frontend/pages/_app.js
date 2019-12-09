d87e838e6760943bd3418b0c6b801d72c9585a80
Alexei Shilin <alexei.shilin@gmail.com>
2019-12-09 17:49:46 +0700

    wip1

---
+++ sick-fits/frontend/pages/_app.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
+import App, { Container } from "next/app";
+import Page from "../components/Page";
+
+class MyApp extends App {
+  render() {
+    const { Component } = this.props;
+
+    return (
+      <Container>
+        <Page>
+          <Component />
+        </Page>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+
+const Sell = props => (
+  <div>
+    <p>Selling it!</p>
+  </div>
+)
+
+export default Sell;
