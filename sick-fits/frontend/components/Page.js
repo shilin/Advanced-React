4375b55bc2c195ef81e353676b2f24c88c7034e2
Alexei Shilin <alexei.shilin@gmail.com>
2019-12-09 19:05:23 +0700

    intro to styled components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,16 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import Meta from './Meta';
+import React, { Component } from "react";
+import Header from "./Header";
+import Meta from "./Meta";
+import styled from "styled-components";
+
+const MyButton = styled.button`
+  background: red;
+  font-size: 50px;
+  .poop {
+    font-size: ${props => props.huge ? '200px' : '30px' };
+  }
+`;
+
 
 class Page extends Component {
   render() {
@@ -8,6 +18,9 @@ class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <MyButton huge>
+          <span className="poop">💩</span>
+        </MyButton>
         {this.props.children}
       </div>
     );
