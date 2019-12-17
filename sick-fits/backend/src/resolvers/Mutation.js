4c7068c387d8a872c1b3d9c36f961dc56538d331
Alexei Shilin <alexei.shilin@gmail.com>
2019-12-17 18:49:14 +0700

    appolo fst step

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
 module.exports = Mutation;
